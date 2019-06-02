4a385988624085ec41572bcadf881dfef4edaf87
DJ.HAN <djhan@kbdmania.net>
2019-06-02 22:24:15 +0900

    first commit

---
+++ AnimationImage/AnimationImage.h
@@ -0,0 +1,19 @@
+//
+//  AnimationImage.h
+//  AnimationImage
+//
+//  Created by DJ.HAN on 02/06/2019.
+//  Copyright © 2019 DJ.HAN. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for AnimationImage.
+FOUNDATION_EXPORT double AnimationImageVersionNumber;
+
+//! Project version string for AnimationImage.
+FOUNDATION_EXPORT const unsigned char AnimationImageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AnimationImage/PublicHeader.h>
+
+
