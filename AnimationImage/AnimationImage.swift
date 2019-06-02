535f76f0f5a26dbef31aca42e4e995576abb9528
DJ.HAN <djhan@kbdmania.net>
2019-06-02 22:27:13 +0900

    make first swift file

---
+++ AnimationImage/AnimationImage.swift
@@ -0,0 +1,13 @@
+//
+//  AnimationImage.swift
+//  AnimationImage
+//
+//  Created by DJ.HAN on 02/06/2019.
+//  Copyright © 2019 DJ.HAN. All rights reserved.
+//
+
+import Cocoa
+
+class AnimationImage: NSObject {
+
+}
