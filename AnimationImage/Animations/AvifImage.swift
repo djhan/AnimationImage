b58d3b403c058770a872b60f2aae18f2c5447a7e
djhan <djhan@kbdmania.net>
2022-09-23 17:16:48 +0900

    ADD Avif Package in AnimatinoImage #2

--- AnimationImage/Animations/AvifImage.swift
+++ AnimationImage/Animations/AvifImage.swift
@@ -27,6 +27,33 @@ class AvifImageClass: DefaultAnimationImage, AnimationConvertible {
     /// EXIF
     var exifData: AnimationExifData?
     
+    
+    // MARK: Initialization
+    /// 초기화
+    init(from imageSource: SDImageAVIFCoder) {
+        super.init()
+        // 이미지 소스 대입
+        self.imageSource = imageSource
+        // 소스 설정시 avif 로 설정
+        self.type = .avif
+    }
+    /// URL로 초기화
+    convenience init?(from url: URL) {
+        // 이미지 소스 생성 실패시 nil 반환
+        guard let data = try? Data.init(contentsOf: url),
+            let imageSource = SDImageAVIFCoder.init(animatedImageData: data) else { return nil }
+        // 정상적으로 초기화
+        self.init(from: imageSource)
+    }
+    
+    /// Data로 초기화
+    convenience init?(from data: Data) {
+        // 이미지 소스 생성 실패시 nil 반환
+        guard let imageSource = SDImageAVIFCoder.init(animatedImageData: data) else { return nil }
+        // 정상적으로 초기화
+        self.init(from: imageSource)
+    }
+    
     /// 지연 시간
     func delayTime(at index: Int) -> Float {
         return Float(self.imageSource?.animatedImageDuration(at: UInt(index)) ?? 0)
